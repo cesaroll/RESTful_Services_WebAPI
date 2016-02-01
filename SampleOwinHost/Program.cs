ab6dce678604774fe3eb885d5a5d3e80a5da8bc8
Cesar Lopez <cesarl@outlook.com>
2016-02-01 09:31:00 -0700

    S07-L32 : Demos on OWIN

---
+++ SampleOwinHost/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+
+namespace SampleOwinHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebApp.Start<StartUp>("http://+:8080"))
+            {
+                Console.WriteLine("OWIN Running ...");
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
---
+++ SampleOwinHost/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web.Http;
+using Owin;
+
+namespace SampleOwinHost
+{
+    public class StartUp
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional}
+                );
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
